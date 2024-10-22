6aaa6dfc3e732331a0f7e99431e4d79666efdb20
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2024-10-22 20:48:14 +0600

    lesson 7

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from aiogram import executor
 import logging
 from config import dp, bot, Admins
-from handlers import commands, echo, quiz, FSM_reg, FSM_Store, send_products, send_and_delete_products
+from handlers import (commands, echo, quiz, FSM_reg, FSM_Store,
+                      send_products, send_and_delete_products,
+                      edit_products)
 from db import db_main
 from handlers.send_products import sendall_products
 
@@ -19,6 +21,7 @@ FSM_Store.register_handlers_store(dp)
 
 send_products.register_send_products_handler(dp)
 send_and_delete_products.register_send_and_products_handler(dp)
+edit_products.register_update_handler(dp)
 
 echo.register_handlers_echo(dp)
 
